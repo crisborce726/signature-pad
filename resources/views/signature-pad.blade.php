a3e9b306893dc064c2fae677b286cde4f218428b
crisborce726 <122069309+crisborce726@users.noreply.github.com>
2023-01-06 11:09:15 +0800

    Update Blade

--- resources/views/signature-pad.blade.php
+++ resources/views/signature-pad.blade.php
@@ -50,6 +50,7 @@
             </div>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/signature_pad@2.3.2/dist/signature_pad.min.js"></script>
     <script type="text/javascript">
         var sig = $('#sig').signature({syncField: '#signature', syncFormat: 'PNG'});
         $('#clear').click(function (e) {
